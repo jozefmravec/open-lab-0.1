1467ee545c61cd09377c4d88bd5834285d9052af
jozefmravec <56106374+jozefmravec@users.noreply.github.com>
2019-10-12 21:47:02 +0200

    i have done sum + 1 job

---
+++ Open-Lab-0.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Open_Lab_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo = 9;
+            cislo++;
+            Console.WriteLine(cislo);
+        }
+    }
+}
