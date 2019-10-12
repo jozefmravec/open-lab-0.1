701eb0f2a98e6309449501905af9bfadb26a3a76
jozefmravec <jojomravec@gmail.com>
2019-10-12 19:13:21 +0200

    Initial

---
+++ Open-Lab-0.1/Open-Lab-0.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_0._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Xamarin Lab");
+        }
+    }
+}
