54bb3d887c01ff20da9dd1bf79f90474dd5eb393
jozefmravec <56106374+jozefmravec@users.noreply.github.com>
2019-10-13 19:52:18 +0200

    I have done fourth exercise

---
+++ Open-Lab-0.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Open_Lab
+{
+    class Program
+    {
+        public static int TriArea(int strana, int vyska)
+        {
+            return (strana * vyska) / 2;                
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TriArea(12, 8));
+        }
+    }
+}
