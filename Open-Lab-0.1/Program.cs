0e0af4d4b457c95ab2687a4dc6df1b2a68709de6
jozefmravec <56106374+jozefmravec@users.noreply.github.com>
2019-10-14 09:26:02 +0200

    Add files via upload

---
+++ Open-Lab-0.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OpenLab
+{
+    class Program
+    {
+        public static double Remainder(double number1, double number2) 
+        {
+            return (number1 % number2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Remainder(1, 3));
+        }
+    }
+}
