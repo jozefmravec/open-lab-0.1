8a32d931c4db7bfed574b42ead8247b9a199da1e
jozefmravec <56106374+jozefmravec@users.noreply.github.com>
2019-10-28 10:03:33 +0100

    Add files via upload

---
+++ Open-Lab-0.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Christmas christmas = new Christmas();
+            Console.WriteLine(christmas.IsTimeForMilkAndCookies(12, 24));
+        }
+    }
+}
