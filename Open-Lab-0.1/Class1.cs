cba6aed88118de7376746292a5ab9e84d67ea30f
jozefmravec <56106374+jozefmravec@users.noreply.github.com>
2019-11-07 19:33:00 +0100

    Open-Lab 3.1

---
+++ Open-Lab-0.1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        public void Array()
+        {
+            int[,] array = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            Console.WriteLine(array[1, 2]);
+        }
+    }
+}
